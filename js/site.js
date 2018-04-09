b48ba958a95e94d4b3760d679b7e9102cd916ff5
DESKTOP-NSQJ6O6\Hari <hari@haridecoded.com>
2018-04-09 16:52:32 -0400

    added demo site

---
+++ js/site.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#myvideo").width($(window).width() * 0.7);
+    $("#myvideo").height($(window).height() * 0.8);  
+    $("#myvideo").css('marginLeft', $(window).width() *0.08 + 'px');
+})
